ad14a3182209a78fe1184fccb785e880f2972878
Josh Whelan <josh.d.whelan@gmail.com>
2019-03-12 23:57:55 +0000

    final commit; stage 1 complete for tinyAppProject

--- express_server.js
+++ express_server.js
@@ -1,19 +1,26 @@
-var express = require("express");
-var app = express();
-var PORT = 8080;
+//module reqs and port/app settings
+const express = require("express");
 
-const generateRandomString = () => Math.random().toString(36).substr(2,8);
+const PORT = 8080;
 
-var urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
-};
-
+const app = express();
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+//used for: shortURL generator
+const generateRandomString = () => Math.random().toString(36).substr(2,8);
+
+//"database" for shortURL/longURL key pairs
+const urlDatabase = {
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xK": "http://www.google.com"
+};
+
+
+//get handlers
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -35,11 +42,6 @@ app.get("/urls/new", (req, res) => {
     res.render("urls_new");
 });
 
-// app.get("/urls/:whatever", (req, res) => {
-//     let templateVars = { whatever: req.params.whatever};
-//     res.render("urls_show", templateVars);
-// });
-
 app.get("/urls/:shortURL", (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
     res.render("urls_show", templateVars);
@@ -49,12 +51,15 @@ app.get("/u/:shortURL", (req, res) => {
     res.redirect(longURL);
   });
 
+//post handler with redirects
 app.post("/urls", (req, res) => {
     let shortUrlKey = generateRandomString();
     let longURL = req.body.longURL; 
     urlDatabase[shortUrlKey] = longURL; 
     res.redirect(`/urls/:${shortUrlKey}`);      
 }); 
+
+
 
 
 app.listen(PORT, () => {
