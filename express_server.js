946ff88120b9d8195516dbcc333c0b5471ba9e45
Josh Whelan <josh.d.whelan@gmail.com>
2019-03-12 18:23:00 +0000

    added ejs file

--- express_server.js
+++ express_server.js
@@ -15,11 +15,16 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
-  });
+});
 
-  app.get("/hello", (req, res) => {
+app.get("/urls", (req, res) => {
+    let templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+});
+
+app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
-  });
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
