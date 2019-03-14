aa34450e082037e53c3433f470b5edab1791439c
Josh Whelan <josh.d.whelan@gmail.com>
2019-03-14 18:08:21 +0000

    added registration page; registration.ejs

--- express_server.js
+++ express_server.js
@@ -1,25 +1,34 @@
-//port used for dev/testing
-const PORT = 8080;
-
-//framework, parsing and template engine specs
 const express = require("express");
-const app = express();
-app.set("view engine", "ejs");
-
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+const uuidv1 = require('uuid/v1');
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 
-const cookieParser = require('cookie-parser')
+const app = express();
+const PORT = process.env.port || 8080;
+//KILL PORT:
+// netstat -ntlp | grep :8080 (<== portnumber)
+// then kill (pid #; provided from above)
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 app.use(cookieParser())
+
 
 //used for: shortURL generator
 const generateRandomString = () => Math.random().toString(36).substr(2,8);
 
-//"database" for shortURL/longURL key pairs
+//user database
+const userDatabase = {
+
+};
+//url database
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
+    "9sm5xK": "http://www.google.com",
 };
+
+
 
 //get handlers 
 app.get("/urls", (req, res) => {
@@ -29,12 +38,14 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
 });
 
+//This renders the new page
 app.get("/urls/new", (req, res) => {
     let templateVars = {
         username: req.cookies["username"]}
     res.render("urls_new", templateVars);
 });
 
+//This renders the show page
 app.get("/urls/:shortURL/", (req, res) => {
     let templateVars = { 
         shortURL: req.params.shortURL, 
@@ -43,27 +54,34 @@ app.get("/urls/:shortURL/", (req, res) => {
     res.render("urls_show", templateVars);
 });
 
+//This redirects to the longURL
 app.get("/u/:shortURL", (req, res) => {
     const longURL = urlDatabase[req.params.shortURL];
     res.redirect(longURL);
 });
 
-//post handlers (with redirects where applicable)
+app.get("/register", (req, res) => {
+
+    res.render("registration", { username: null });
+});
+
+//This generates short URL for an entered longURL and redirects to the urls/ page
 app.post("/urls", (req, res) => {
     let shortUrlKey = generateRandomString();
     let longURL = req.body.longURL; 
     urlDatabase[shortUrlKey] = longURL; 
-    console.log(shortUrlKey);
-    // res.redirect(`/urls/${shortUrlKey}`);      
+    res.redirect(`/urls/${shortUrlKey}`);      
 }); 
     
-//Update (using post in place of put) to update a longURL
+//This updates a longURL; allows user to edit longURL
 app.post("/urls/:shortURL", (req, res) => {
     var formContent = req.body.longURL;
-    urlDatabase[req.params.shortURL] = formContent; 
+    var shortURL = req.params.shortURL;
+    urlDatabase[shortURL] = formContent; 
     res.redirect('/urls');
 }); 
-    
+
+//This logs my user in
 app.post("/login", (req, res) => {
     let username = req.body.username;
     res.cookie('username', username);
@@ -77,12 +95,13 @@ app.post("/urls/:shortURL/delete", (req, res) => {
     res.redirect("/urls");
 });
 
+//This logs out the user
 app.post("/logout", (req, res) => {
     let username = req.body.username;
     res.clearCookie('username');
     res.redirect("/urls");
 })
-//app is listening
+//This shows me that my app is listening
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
